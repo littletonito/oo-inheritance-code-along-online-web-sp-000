6c4f2b84a08d72ed47c28afad42521f962e64f55
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-03 15:26:16 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+#By requiring the file we can inherit all of the methods and ifo from that class
+#Car < (inherits from) Vehicle
+class Car < Vehicle
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
